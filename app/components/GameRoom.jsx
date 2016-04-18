e77fea00c21338bc8a94647051eddabe0bab932f
Clay Schubiner <clay.schubiner@airbnb.com>
2016-04-18 01:12:21 -0700

    initial quest leader framework

--- app/components/GameRoom.jsx
+++ app/components/GameRoom.jsx
@@ -25,10 +25,60 @@ export default class GameRoom extends React.Component {
     );
   }
 
+  setInitialGameState() {
+    const gameStateRef = new Firebase(`https://avalonline.firebaseio.com/games/${this.props.roomCode}/gameState`);
+    gameStateRef.once("value", (snapshot) => {
+      const gameState = snapshot.val();;
+      if (!gameState || !gameState.questLeader) {
+        gameStateRef.update({questLeader: _.sample(this.props.players).playerName});
+      }
+      if (!gameState || !gameState.numProposals) {
+        gameStateRef.update({numProposals: 0});
+      }
+    });
+  }
+
+  componentDidMount() {
+    const gameStateRef = new Firebase(`https://avalonline.firebaseio.com/games/${this.props.roomCode}/gameState`);
+    gameStateRef.on("value", (snapshot) => {
+      this.setState({'gameState': snapshot.val()})
+    });
+
+    this.setInitialGameState();
+  }
+
+  // the game state that is always displayed
+  getPermanentGameState() {
+    if (!this.state.gameState) return null;
+
+    return (
+      <div>
+        <div>
+          numProposals: {this.state.gameState.numProposals}
+        </div>
+        <div>
+          questLeader: {this.state.gameState.questLeader}
+        </div>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div>
         <h1>Game Room</h1>
+        { this.getPermanentGameState() }
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
         <h2>Up name q: <span className='bold'>{ this.props.playerName }</span></h2>
         <h2>Up role q: <span className='bold'>{ this.getCurrentPlayer().role }</span></h2>
         <YourInfo
