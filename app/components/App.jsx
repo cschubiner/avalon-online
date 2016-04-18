4b0614292d9956521252494933b48e7584314279
Clay Schubiner <clay.schubiner@airbnb.com>
2016-04-17 17:16:21 -0700

    map

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,44 @@
 import React from 'react';
 import Note from './Note.jsx';
+import Firebase from 'firebase';
+import _ from 'lodash';
+
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+    }
+  }
+
+  componentDidMount() {
+    // this.currentTrackURIRef.on("value", (dataSnapshot) => {
+    //   let trackURI = dataSnapshot.val();
+    //   console.log(trackURI);
+    // });
+  }
+
+  getNewRoomCode() {
+    return _.sample(
+      ['jizz', 'bang', 'joks', 'cary']
+    );
+  }
+
+  newGameClicked() {
+    const roomCode = this.getNewRoomCode();
+    const fbGame = new Firebase(`https://avalonline.firebaseio.com/games/${roomCode}`);
+    fbGame.set({
+      'roomCode': roomCode,
+    });
+  }
+
   render() {
-    return <Note />;
+    return (
+      <div>
+        <button type="button" onClick={this.newGameClicked.bind(this)}>
+          New Game
+        </button>
+      </div>
+    );
   }
 }
